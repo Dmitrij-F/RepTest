4a400d33ef9b95c87d450716f2bf75126a5ab210
Dmitrij-F <atfdaj@gmail.com>
2015-09-04 00:05:18 +0400

    push

--- MainModulGit/src/DopGit.java
+++ MainModulGit/src/DopGit.java
@@ -1,5 +1,16 @@
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Created by Bro on 03.09.15.
  */
 public class DopGit {
+void stream (){
+
+    try {
+        System.in.read(null);
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
 }

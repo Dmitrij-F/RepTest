9caadfefc21be5c818db91296d23a08a0c47bbf6
Dmitrij-F <atfdaj@gmail.com>
2015-09-03 14:31:52 +0400

    commit 3

--- MainModulGit/src/MainGit.java
+++ MainModulGit/src/MainGit.java
@@ -1,9 +1,9 @@
 
 public class MainGit {
     int c = 1;
-    boolean b = true;
     public static void main(String[] args) {
         MainGit oc = new MainGit();
+
         //oc.c<<=2;
         System.out.println("hi Git! "+oc.c);
     }

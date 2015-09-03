bb3413b77cfd8cfe8ab3e03a774dd531bda4baa5
Dmitrij-F <>
2015-09-03 14:03:24 +0400

    New changelist

--- MainModulGit/src/MainGit.java
+++ MainModulGit/src/MainGit.java
@@ -2,6 +2,9 @@
 public class MainGit {
     int c = 1;
     public static void main(String[] args) {
-        System.out.println("hi Git!");
+        MainGit oc = new MainGit();
+
+        //oc.c<<=2;
+        System.out.println("hi Git! "+oc.c);
     }
 }

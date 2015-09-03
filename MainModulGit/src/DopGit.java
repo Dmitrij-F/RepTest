461fa287c2b346d88e6ca8f5e32defeeaa7c1b13
Dmitrij-F <>
2015-09-03 14:28:21 +0400

    commit 3

---
+++ MainModulGit/src/DopGit.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Bro on 03.09.15.
+ */
+public class DopGit {
+    String s = "attack";
+}
--- MainModulGit/src/MainGit.java
+++ MainModulGit/src/MainGit.java
@@ -1,6 +1,7 @@
 
 public class MainGit {
     int c = 1;
+    boolean b = true;
     public static void main(String[] args) {
         MainGit oc = new MainGit();
         //oc.c<<=2;

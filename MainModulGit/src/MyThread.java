9445d5d47b4656182c7e80038b7d8b7ca02cfdeb
Dmitrij-F <atfdaj@gmail.com>
2015-10-06 22:27:21 +0400

    another commit

---
+++ MainModulGit/src/MyThread.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Bro on 05.10.15.
+ */
+public class MyThread implements Runnable {
+    private int x;
+    private int y;
+
+    @Override
+    public  void run() {
+        for (;;) /* Line 13 */
+        {
+            x++;
+            y++;
+            System.out.println("x = " + x + "y = " + y);
+        }
+    }
+}
